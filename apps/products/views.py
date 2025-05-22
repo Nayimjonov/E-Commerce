784ed03084f2c005389fddc097da55d8fffad266
Samandar <samandarnayimjonov@gmail.com>
2025-05-22 12:30:30 +0500

    product views done

---
+++ apps/products/views.py
@@ -0,0 +1,18 @@
+from rest_framework.generics import ListAPIView
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Product
+from .serializers import ProductSerializer
+from .paginations import ProductPagination
+from .filters import ProductFilter
+
+
+class ProductListView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = ProductPagination
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = ProductFilter
+    search_fields = ['title', 'description']
+    ordering_fields = ['price', 'created_at', 'title', 'average_rating']
+    ordering = ['title']  # по умолчанию
