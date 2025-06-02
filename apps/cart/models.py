5e634fb1b4e297b05f5f5f92b69afaf14462d883
Samandar <samandarnayimjonov@gmail.com>
2025-06-02 08:36:30 +0500

    cart model done

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from products.models import Product
 
-# Create your models here.
+User = get_user_model()
+
+
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        unique_together = ('user', 'product')
+
+    def subtotal(self):
+        return self.product.price * self.quantity
