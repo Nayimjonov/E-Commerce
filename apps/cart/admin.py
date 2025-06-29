f9d8a3655af1ad9f1b89c48768cbf68985f10c23
Samandar <samandarnayimjonov@gmail.com>
2025-06-29 08:24:07 +0500

    cart done

--- apps/cart/admin.py
+++ apps/cart/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CartItem
 
-# Register your models here.
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'product', 'quantity', 'subtotal_display')
+    search_fields = ('user__username', 'product__title')
+    list_filter = ('user',)
+
+    def subtotal_display(self, obj):
+        return obj.subtotal()
+    subtotal_display.short_description = 'Сумма'
